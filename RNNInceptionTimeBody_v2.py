16d71a2a913fb363c1cdfbb74e0f14c9c1cd1f62
Alim <42492971+alxxtexxr@users.noreply.github.com>
2025-04-20 23:19:42 +0700

    Update RNNInceptionTimeBody_v2.py

--- RNNInceptionTimeBody_v2.py
+++ RNNInceptionTimeBody_v2.py
@@ -11,12 +11,12 @@ def ifnone(a, b):
 class _RNNInceptionTimeBody_v2_Base(torch.nn.Module):
     def __init__(self,
                  c_in, c_out, seq_len,
-                 # Body Metrics 
+                 # Body metrics parameters
                  body_c_in, body_nf=128,
-                 # RNN  
+                 # RNN parameters
                  hidden_size=128, rnn_layers=1, bias=True, cell_dropout=0, rnn_dropout=0.8, bidirectional=False, shuffle=True, 
                  fc_dropout:float=0.,
-                 # InceptionTime
+                 # InceptionTime parameters
                  nf=32, 
                  ):
         super().__init__()
@@ -24,23 +24,23 @@ class _RNNInceptionTimeBody_v2_Base(torch.nn.Module):
         inception_head_nf = nf * 4
         assert  body_nf == (hidden_size * (1 + bidirectional)) == inception_head_nf
         
-        # RNN
+        # RNN layers
         self.rnn = self._cell(seq_len if shuffle else c_in, hidden_size, num_layers=rnn_layers, bias=bias, batch_first=True, 
                               dropout=cell_dropout, bidirectional=bidirectional)
         self.rnn_dropout = torch.nn.Dropout(rnn_dropout) if rnn_dropout else noop
         self.shuffle = Permute(0, 2, 1) if not shuffle else noop # You would normally permute x. Authors did the opposite.
         
-        # InceptionTime
+        # InceptionTime layers
         self.inception_block = InceptionBlock(c_in, nf)
         
         self.gap = GAP1d(1)
         
-        # Head
+        # Head layers
         self.concat = Concat()
         self.fc_dropout = torch.nn.Dropout(fc_dropout) if fc_dropout else noop
         self.fc = torch.nn.Linear((hidden_size * (1 + bidirectional)) + inception_head_nf, c_out)
 
-        # Body Metrics
+        # Body metrics layers
         self.body_block = torch.nn.Sequential(
             torch.nn.Linear(body_c_in, body_nf),
             torch.nn.ReLU(),
@@ -53,22 +53,22 @@ class _RNNInceptionTimeBody_v2_Base(torch.nn.Module):
         )
 
     def forward(self, x, x_body):
-        # Body
+        # Body metrics forward
         body_out = self.body_block(x_body)
 
-        # RNN
+        # RNN forward
         rnn_in = self.shuffle(x) # permute --> (batch_size, seq_len, n_vars) when batch_first=True
         rnn_out, _ = self.rnn(rnn_in)
         rnn_out = rnn_out[:, -1] # output of last sequence step (many-to-one)
         rnn_out = self.rnn_dropout(rnn_out)
         rnn_out += body_out
         
-        # InceptionTime
+        # InceptionTime forward
         inception_out = self.inception_block(x)
         inception_out = self.gap(inception_out)
         inception_out += body_out
         
-        # Head
+        # Head forward
         x = self.concat([rnn_out, inception_out])
         x = self.fc_dropout(x)
         x = self.fc(x)
