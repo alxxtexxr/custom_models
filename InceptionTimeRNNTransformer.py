a5961d9a70df8ead4262bf273bd788a8d5c3e055
Alim <42492971+alxxtexxr@users.noreply.github.com>
2025-04-22 22:11:54 +0700

    Update InceptionTimeRNNTransformer.py

--- InceptionTimeRNNTransformer.py
+++ InceptionTimeRNNTransformer.py
@@ -7,31 +7,31 @@ from tsai.models.layers import Flatten, SigmoidRange
 class _InceptionTimeRNNTransformer_Base(torch.nn.Module):
     def __init__(
         self, c_out, seq_len,
-        # InceptionTime
+        # InceptionTime parameters
         c_in, nf=32, 
-        # RNN
+        # RNN parameters
         hidden_size=128, rnn_layers=1, bias=True, rnn_dropout=0, bidirectional=False,
-        # Transformer
+        # Transformer parameters
         encoder_layers:int=3, n_heads:int=16, d_k=None, d_v=None, d_ff=256, encoder_dropout=0.1, act="gelu", 
-        # Head
+        # Head parameters
         fc_dropout=0., y_range=None,
     ):
         super().__init__()
         
-        # InceptionTime
+        # InceptionTime layers
         self.inceptionblock = InceptionBlock(c_in, nf)
         # self.gap = GAP1d(1)
         
-        # RNN
+        # RNN layers
         q_len = seq_len
         self.rnn = self._cell(nf * 4, hidden_size, num_layers=rnn_layers, bias=bias, batch_first=True, dropout=rnn_dropout, 
                               bidirectional=bidirectional)
         
-        # Transformer
+        # Transformer layers
         d_model = hidden_size * (1 + bidirectional)
         self.encoder = _TSTEncoder(q_len, d_model, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, dropout=encoder_dropout, activation=act, n_layers=encoder_layers)
         
-        # Head
+        # Head layers
         self.head_nf = q_len * d_model
         self.head = self.create_head(self.head_nf, c_out, act=act, fc_dropout=fc_dropout, y_range=y_range)
     
@@ -43,18 +43,18 @@ class _InceptionTimeRNNTransformer_Base(torch.nn.Module):
         return torch.nn.Sequential(*layers)    
     
     def forward(self, x):
-        # InceptionTime
+        # InceptionTime forward
         x = self.inceptionblock(x)
 
-        # RNN
+        # RNN forward
         x = x.transpose(2, 1)   # [bs x nvars x q_len] --> [bs x q_len x nvars]
         output, _ = self.rnn(x) # output from all sequence steps: [bs x q_len x hidden_size * (1 + bidirectional)]
         
-        # Transformer
+        # Transformer forward
         z = self.encoder(output)           # z: [bs x q_len x d_model]
         z = z.transpose(2, 1).contiguous() # z: [bs x d_model x q_len]
         
-        # Head
+        # Head forward
         output = self.head(z) # output: [bs x c_out]
         return output
 
